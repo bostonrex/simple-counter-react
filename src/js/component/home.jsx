67d974e7ada205ebfd6fcac58407f1ba94c2ae2d
bostonrex <reynaldogoitia@gmail.com>
2023-03-15 20:06:02 +0000

    cambios en estilo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,13 +6,13 @@ const Home = ({ first, second, third, fourth, fifth, sixth }) => {
 
 	return (
 		<div className="row bg-black col-7 text-center d-flex justify-content-center mx-auto p-2 mt-3 ">
-			<div className="col bg-dark text-white fs-1 border border-dark p-4 rounded-1 border border-1 m-2 fw-bold" ><i class="far fa-clock"></i></div>
-			<div className="col bg-dark text-white fs-1 border border-dark p-4 rounded-1 border border-1 m-2 fw-bold ">{sixth}</div>
-			<div className="col bg-dark text-white fs-1 border border-dark p-4 rounded-1 border border-1 m-2 fw-bold">{fifth}</div>
-			<div className="col bg-dark text-white fs-1 border border-dark p-4 rounded-1 border border-1 m-2 fw-bold">{fourth}</div>
-			<div className="col bg-dark text-white fs-1 border border-dark p-4 rounded-1 border border-1 m-2 fw-bold">{third}</div>
-			<div className="col bg-dark text-white fs-1 border border-dark p-4 rounded-1 border border-1 m-2 fw-bold">{second}</div>
-			<div className="col bg-dark text-white fs-1 border border-dark p-4 rounded-1 border border-1 m-2 fw-bold">{first}</div>
+			<div className="col bg-dark text-white fs-1 border border-dark p-4 rounded-1 m-2" ><i class="far fa-clock"></i></div>
+			<div className="col bg-dark text-white fs-1 border border-dark p-4 rounded-1 border border-1 m-2">{sixth}</div>
+			<div className="col bg-dark text-white fs-1 border border-dark p-4 rounded-1 border border-1 m-2">{fifth}</div>
+			<div className="col bg-dark text-white fs-1 border border-dark p-4 rounded-1 border border-1 m-2">{fourth}</div>
+			<div className="col bg-dark text-white fs-1 border border-dark p-4 rounded-1 border border-1 m-2">{third}</div>
+			<div className="col bg-dark text-white fs-1 border border-dark p-4 rounded-1 border border-1 m-2">{second}</div>
+			<div className="col bg-dark text-white fs-1 border border-dark p-4 rounded-1 border border-1 m-2">{first}</div>
 		</div>
 	);
 
